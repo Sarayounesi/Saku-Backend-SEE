d9cb8d258f41a7e02d6bf3d4a3369afcb5afa034
Shahrzad <shahrzad123azari@gmail.com>
2022-03-06 21:20:03 +0330

    set jwt

--- saku/saku/settings.py
+++ saku/saku/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'auction'
 ]
 
@@ -99,6 +100,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+     ],
+}
 
 
 # Internationalization
