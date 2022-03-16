4375aa5015740c65685ddac9d73b990de15ed80c
Shahrzad Azari <56067208+ShahrzadAzari@users.noreply.github.com>
2022-03-16 11:46:09 +0330

    update db host

--- saku/saku/settings.py
+++ saku/saku/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'saku-db',
         'USER': 'postgres',
         'PASSWORD': 'saku1234',
-        'HOST': 'db',
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': '5432',
     }
 }
