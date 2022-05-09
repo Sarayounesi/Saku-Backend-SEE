b96ac8aea2f1aeea5136aa001aa874e5ba5935d2
Shahrzad <shahrzad123azari@gmail.com>
2022-05-09 17:14:18 +0430

    check email uniqueness when updating profile

--- saku/user_profile/serializers.py
+++ saku/user_profile/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from .models import Profile
 
 
@@ -9,3 +10,10 @@ class ProfileSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "user": {"read_only": True}
         }
+
+    def validate(self, data):
+        email = data.get('email')
+        if email:
+            if len(Profile.objects.filter(email=email))>0:
+                raise serializers.ValidationError("Another user exists with this email address.")
+        return data
--- saku/user_profile/views.py
+++ saku/user_profile/views.py
@@ -10,9 +10,14 @@ class UpdateProfile(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
 
     def get_object(self):
+        serializer = self.get_serializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+
         profile = Profile.objects.filter(user = self.request.user)
         if len(profile) != 0:
             queryset = profile[0]
         else:
             queryset = Profile.objects.create(user=self.request.user, national_id='0', email='')
         return queryset
+
+    
