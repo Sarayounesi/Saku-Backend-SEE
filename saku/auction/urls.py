26e9d454e8d756042d354165d549a68f814d0ac8
Shahrzad Azari <56067208+ShahrzadAzari@users.noreply.github.com>
2022-07-09 23:17:08 +0430

    Update urls.py

--- saku/auction/urls.py
+++ saku/auction/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from auction.views import CreateListAuction, CategoryList, DetailedAuction
+from auction.views import CreateListAuction, CategoryList, DetailedAuction, DeleteAuctionPicture
 
 urlpatterns = [
     path('', CreateListAuction.as_view(), name="auction"),
     path('categories/', CategoryList.as_view()),
     path('<str:token>', DetailedAuction.as_view(), name="detailed_auction"),
+    path('picture/<str:token>', DeleteAuctionPicture.as_view(), name="delete_auction_picture"),
 ]
