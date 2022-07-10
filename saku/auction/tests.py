69fdfe7107ee481c4875d5098797954077e5101b
Shahrzad <shahrzad123azari@gmail.com>
2022-07-11 01:55:30 +0430

    fix best_bid test

--- saku/auction/tests.py
+++ saku/auction/tests.py
@@ -6,7 +6,7 @@ from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from rest_framework import status
 from auction.models import Auction, Tags, Category
-form bid.models import Bid
+from bid.models import Bid
 
 
 # Create your tests here.
