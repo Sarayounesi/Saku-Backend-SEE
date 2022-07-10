e00efe11a2e359b55ef5598fe1ea7eda4896b9e0
Mehdi Mostafavi <mehdi.mostafavi@divar.ir>
2022-07-10 10:24:23 +0430

    Add online bid

--- saku/bid/models.py
+++ saku/bid/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Bid(models.Model):
-    time = models.DateTimeField(auto_created=True)
+    time = models.DateTimeField(auto_now=True)
     price = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     auction = models.ForeignKey(to='auction.Auction', on_delete=models.CASCADE)
